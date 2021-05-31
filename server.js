5a854f0c9c6f21810034787794a7977c2cfacc38
Steve Babb <steve.babb@outlook.com>
2021-05-31 15:57:35 -0500

    add first component

---
+++ server.js
@@ -0,0 +1,20 @@
+// server/index.js
+
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
